5b6637f19f85cb56c890393b8eaf383586c15d94
Phrygan <honga010807@gmail.com>
2024-05-29 14:45:26 -0400

    finished profile

--- code-ninjas-website/src/components/Profile/Profile.js
+++ code-ninjas-website/src/components/Profile/Profile.js
@@ -1,9 +1,17 @@
 import './Profile.css'
+import profilePicture from '../../assets/profilepicture.jpg'
 
 function Profile(props) {
     return (
         <div id="ProfileContainer">
-            <h1>I'm a profile</h1>
+            <h1 id="ProfileTitle">Profile</h1>
+            <div id="UserProfile">
+                <img id="ProfilePicture" src={profilePicture} alt="Profile Picture"/>
+                <h1 id="Name" className="Description">Andrew Hong</h1>
+                <h3 id="Join" className="Description">Joined Mar 2022</h3>
+                <br></br>
+                <h1 id="NinjaBucks" className="Description">2000 NB</h1>
+            </div>
         </div>
     )
 }
