6fccf118360a4e8110e8423d8dd40b1955c4357b
Phrygan <honga010807@gmail.com>
2024-06-03 13:32:00 -0400

    need to merge

--- code-ninjas-website/src/App.js
+++ code-ninjas-website/src/App.js
@@ -4,24 +4,42 @@ import Profile from './components/Profile/Profile.js'
 import Tasks from './components/Tasks/Tasks.js'
 import Login from './components/Login.js'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import useToken from "./components/useToken.js";
 
 function App() {
+  const { token, removeToken, setToken } = useToken();
+  
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={
             <>
-              <Navbar />
-              <div id="Main">
-                <div className="Tab">
-                  <Profile />
-                </div>
-                <div className="Tab">
-                  <Tasks/>
-                </div>
-              </div>
+              { token ?
+              (
+                <>
+                  <Routes>
+                    <Route path="/student" element={
+                    <>
+                      <Navbar />
+                      <div id="Main">
+                        <div className="Tab">
+                          <Profile />
+                        </div>
+                        <div className="Tab">
+                          <Tasks/>
+                        </div>
+                      </div>
+                    </>}></Route>
+                  </Routes>
+                </>
+              ) : (
+                <>
+                  <LoginPage setToken={setToken} />
+                  <h1>{token}</h1>
+                </>
+              )
+              }
             </>
           }/>
           <Route path="/login" element={
