4688c5a55fdb4f4edb15e3416df08776cb0755f7
Phrygan <honga010807@gmail.com>
2024-06-03 12:00:50 -0400

    backend complete and working with postman

--- code-ninjas-website/api/app.py
+++ code-ninjas-website/api/app.py
@@ -2,24 +2,67 @@ import json
 from flask import Flask, request, jsonify
 from datetime import datetime, timedelta, timezone
 from flask_jwt_extended import create_access_token, get_jwt, get_jwt_identity, unset_jwt_cookies, jwt_required, JWTManager
+import os
+from pymongo import MongoClient
+from passlib.hash import pbkdf2_sha256
+from dotenv import load_dotenv, find_dotenv
 
+
+# flask
 api = Flask(__name__)
 
 api.config["JWT_SECRET_KEY"] = "xwmq1cf4xqkjmv4tcnh8hepa822n8yog"
 api.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 jwt = JWTManager(api)
 
+# db
+load_dotenv(find_dotenv())
+password = os.environ.get("MONGODB_PWD")
+
+connection_string = f"mongodb+srv://CodeNinjas:{password}@codeninjas.lppiwo8.mongodb.net/?retryWrites=true&w=majority&appName=codeninjas"
+client = MongoClient(connection_string)
+
+databases = client.list_database_names()
+userLogin_database = client.loginData
+userData_database = client.userData
+
+# sign up
+@api.route('/signup', methods=["POST"])
+def signup():
+    user = {
+        "email": request.json.get("email"),
+        "password": request.json.get("password")
+    }
+
+    user["password"] = pbkdf2_sha256.encrypt(user["password"])
+
+    if userLogin_database.users.find_one({"email": user["email"]}):
+        return {"msg": "Email already in use"}, 400
+
+    if userLogin_database.users.insert_one(user):
+        return {"msg": "Sign up succeeded"}, 200
+    
+    return {"msg": "Error, signup Failed"}, 400
+
+
+# logging in
 @api.route('/token', methods=["POST"])
 def create_token():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
-    if email != "test" or password != "test":
-        return {"msg": "Wrong email or password"}, 401
-    
-    access_token = create_access_token(identity=email)
-    response = {"access_token": access_token}
-    return response
 
+    user = userLogin_database.users.find_one({
+        "email": email
+    })
+
+    if user and pbkdf2_sha256.verify(password, user['password']):
+        access_token = create_access_token(identity=email)
+        response = {"access_token": access_token}
+        return response
+    
+    return {"msg": "Wrong email or password"}, 401
+    
+# logging out
 @api.route("/logout", methods=["POST"])
 def logout():
     response = jsonify({"msg": "logout successful"})
@@ -45,9 +88,14 @@ def refresh_expiring_jwts(response):
 @api.route('/profile', methods=["GET"])
 @jwt_required()
 def my_profile():
-    response_body = {
-        "name": "Nagato",
-        "about": "My about..."
-    }
+    current_user = get_jwt_identity()
+    print(current_user)
+    response_body = userData_database.userData.find_one({
+        "email": current_user
+    })
 
-    return response_body
+    if response_body:
+        response = json.dumps(response_body, default=str)
+        return response
+
+    return {"msg": "Something went wrong with your token"}, 400
--- code-ninjas-website/src/App.js
+++ code-ninjas-website/src/App.js
@@ -2,19 +2,36 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar.js'
 import Profile from './components/Profile/Profile.js'
 import Tasks from './components/Tasks/Tasks.js'
+import Login from './components/Login.js'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <div id="Main">
-        <div className="Tab">
-          <Profile />
-        </div>
-        <div className="Tab">
-          <Tasks/>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Navbar />
+              <div id="Main">
+                <div className="Tab">
+                  <Profile />
+                </div>
+                <div className="Tab">
+                  <Tasks/>
+                </div>
+              </div>
+            </>
+          }/>
+          <Route path="/login" element={
+            <>
+              <Login />
+            </>
+          }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
