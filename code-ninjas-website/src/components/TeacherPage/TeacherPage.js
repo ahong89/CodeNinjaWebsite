b3fc6297f1ba9f14e81a2736afb020c9455a720f
Phrygan <honga010807@gmail.com>
2024-06-05 14:18:19 -0400

    more backend things

--- code-ninjas-website/src/components/TeacherPage/TeacherPage.js
+++ code-ninjas-website/src/components/TeacherPage/TeacherPage.js
@@ -7,44 +7,47 @@ import axios from 'axios';
 
 function TeacherPage(props) {
 
-    const [ studentData, setStudentData ] = useState(() => getData);
+  const [ studentData, setStudentData ] = useState(() => getData());
+  const [ currStudent, setCurrStudent ] = useState(null)
 
-    function getData() {
-        axios({
-          method: "GET",
-          url: "/getallstudents",
-          headers: {
-            Authorization: 'Bearer ' + props.token
-          }
-        })
-        .then((response) => {
-          const res = response.data
-          res.access_token && props.setToken(res.access_token)
-          setStudentData(res)
-          return res
-        }).catch((error) => {
-          if (error.response) {
-            console.log(error.response)
-            console.log(error.response.status)
-            console.log(error.response.headers)
-          }
-        })
-        return ({});
-      }
+  function getData() {
+      axios({
+        method: "GET",
+        url: "/getallstudents",
+        headers: {
+          Authorization: 'Bearer ' + props.token
+        }
+      })
+      .then((response) => {
+        console.log(response.data)
+        const res = response.data
+        res.access_token && props.setToken(res.access_token)
+        setStudentData(res)
+        return res
+      }).catch((error) => {
+        if (error.response) {
+          console.log(error.response)
+          console.log(error.response.status)
+          console.log(error.response.headers)
+        }
+      })
+      return ([]);
+    }
 
-    return (    
-        <div className="TeacherPage">
-          <Navbar removeToken={props.removeToken} setIsAuthenticated={props.setIsAuthenticated}/>
-          <div id="MainContainer">
-              <div className="Tab">
-                  <StudentList studentData={studentData}/>
-              </div>
-              <div className="Tab">
-                  <StudentProfile studentData={studentData}/>
-              </div>
-          </div>
+  return (    
+      <div className="TeacherPage">
+        <Navbar removeToken={props.removeToken} setIsAuthenticated={props.setIsAuthenticated}/>
+        <div id="MainContainer">
+            <div className="Tab">
+                <StudentList studentData={studentData} setCurrStudent={setCurrStudent}/>
+            </div>
+            <div className="Tab">
+                <StudentProfile token={props.token} studentData={studentData} setCurrStudent={setCurrStudent} currStudent={currStudent}/>
+            </div>
         </div>
-    )
+        <button onClick={() => console.logx(currStudent)}>Click me</button>
+      </div>
+  )
 }
 
 export default TeacherPage;
